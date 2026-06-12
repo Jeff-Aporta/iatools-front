520ee83556e9601807f61fbd61740cc4b166696c
Jeff-Aporta <jagudeloe@users.noreply.github.com>
2026-06-12 09:13:09 -0500

    Registrar app iatools-front para JWT por aplicación.

--- js/core/isa-setup.ts
+++ js/core/isa-setup.ts
@@ -2,6 +2,7 @@
   "use strict";
   window.ISAFront.registerApp({
     ns: "IAT",
+    app: "iatools-front",
     theme: { lsKey: "iatools:theme" },
     widgets: { targetStyle: "switch" },
     loginGate: "inline",
